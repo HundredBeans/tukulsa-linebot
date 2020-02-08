1317aa668463a92eac0a1cd86ad3a4d69a87732a
Mohammad Daffa <daffa@alterra.id>
2020-02-08 23:20:53 +0700

    remove catatan.py

--- catatan.py
+++
@@ -1,6 +0,0 @@
-import re
-
-pattern_complaint = r"laporkan masalah TUKULSAORDER.?-\d+"
-pattern_order = r"TUKULSAORDER.?-\d+"
-order_id = re.findall(pattern_complaint, "laporkan masalah TUKULSAORDER-1")
-print(order_id)
